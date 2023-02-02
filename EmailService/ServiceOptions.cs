3e6f530df55cb505b019402bbbe4d0e953d92de5
Waseem Ahmad Mughal <WasimAhmad@users.noreply.github.com>
2023-02-02 23:22:06 +0500

    credentials issue fixed.

--- EmailService/ServiceOptions.cs
+++ EmailService/ServiceOptions.cs
@@ -1,26 +1,13 @@
 ﻿public class ServiceOptions
 {
-    public const string Identifier = "ServiceOptions";
-
-    public SendGridOption SendGridOption { get; set; }
-    public RabitMQOption RabitMQOption { get; set; }
-    
+    public string RabitMQHostName { get; set; }
+    public string RabitMQUserName { get; set; }
+    public string RabitMQPassword { get; set; }
+    public int RabitMQPort { get; set; }
+    public string RabitMQQueueName { get; set; }
+    public string SendGridApiKey { get; set; }
+    public string SendGridFromEmail { get; set; }
+    public string FromName { get; set; }
    
 }
 
-public class RabitMQOption
-{
-    public string HostName { get; set; }
-    public string UserName { get; set; }
-    public string Password { get; set; }
-    public int Port { get; set; }
-    public string QueueName { get; set; }
-
-}
-
-public class SendGridOption
-{
-    public string ApiKey { get; set; }
-    public string FromEmail { get; set; }
-    public string FromName { get; set; }
-}
